d8085dd470d89283cea94d6fc2467d99a265cc3a
jihyun-00 <rkdwl811@naver.com>
2025-04-08 20:55:08 +0900

    충돌 해결

---
+++ TodoList/src/main/java/edu/kh/todoList/model/dao/TodoListDAO.java
@@ -0,0 +1,23 @@
+package edu.kh.todoList.model.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import edu.kh.todoList.model.dto.Todo;
+
+public interface TodoListDAO {
+
+	List<Todo> todoListFullView(Connection conn) throws Exception;
+
+	int getCompleteCount(Connection conn) throws Exception;
+
+	int todoAdd(Connection conn, String title, String detail) throws Exception;
+
+	Todo todoDetail(Connection conn, int todoNo) throws Exception;
+
+	int todoComplete(Connection conn, int todoNo) throws Exception;
+
+	int todoUpdate(Connection conn, int todoNo, String title, String detail) throws Exception;
+
+}
